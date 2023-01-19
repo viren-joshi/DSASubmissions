ffa4d3462e87b3af5e6d08b812cf24803f643d46
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2023-01-19 19:09:25 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         if(nums.size()==2) return vector<int> {0,1};
-        std::vector<int>::iterator i = nums.end();
-        // sort(nums.begin(),nums.end());
-        for (std::vector<int>::iterator itr=nums.begin();itr!=nums.end();itr++){
+        auto i = nums.end();
+        for (auto itr=nums.begin();itr!=nums.end();itr++){
             i = find(itr+1,nums.end(),target-*itr);
-            // cout<<*itr<<" "<<*i;
             if(i!=nums.end() && i!=itr){
                 return vector<int>{int(itr-nums.begin()),int(i-nums.begin())};
             }
