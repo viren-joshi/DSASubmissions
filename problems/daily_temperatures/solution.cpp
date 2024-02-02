2055e19adc7b8e93897e9f80af93b01d19ecef60
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2024-02-02 02:22:01 +0000

    Sync LeetCode submission - Daily Temperatures (cpp)

--- problems/daily_temperatures/solution.cpp
+++ problems/daily_temperatures/solution.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         vector<int> ans(temperatures.size(),0);
-        deque<int> d;
+        stack<int> d;
 
         for(int i = temperatures.size() - 1; i >= 0; i--) {
             if(d.empty()) {
-                d.push_front(i);
+                d.push(i);
                 ans[i] = 0;
             } else {
-                while(!d.empty() && temperatures[i] >= temperatures[d.front()]) {
-                    d.pop_front();
+                while(!d.empty() && temperatures[i] >= temperatures[d.top()]) {
+                    d.pop();
                 }
                 if(d.empty()) {
                     ans[i] = 0;
                 } else {
-                    ans[i] = d.front() - i;
+                    ans[i] = d.top() - i;
                 }
-                d.push_front(i);
+                d.push(i);
             }
         }
         return ans;
