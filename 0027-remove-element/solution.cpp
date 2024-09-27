f263c868f48ed43eb201012d342e0de1cdd743f4
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2024-09-27 14:40:03 +0000

    Sync LeetCode submission Runtime - 3 ms (30.72%), Memory - 10.8 MB (37.76%)

---
+++ 0027-remove-element/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0,j=0;
+        while(i<nums.size()) {
+            if(nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+            i++;
+        }
+        return j;
+    }
+};
