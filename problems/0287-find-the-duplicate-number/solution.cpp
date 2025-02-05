2c9e91303cc6d12fb82db929a09fdd3c27455478
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2025-02-05 19:29:19 +0000

    Sync LeetCode submission Runtime - 2 ms (71.84%), Memory - 64.9 MB (99.24%)

---
+++ problems/0287-find-the-duplicate-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast = nums[0], slow = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (fast != slow);
+        fast = nums[0];
+        while(fast != slow) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
+    }
+};
