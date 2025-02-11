74a75767458da686674fbcfec1cd83911dec5d15
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2025-02-11 14:57:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.4 MB (94.08%)

--- problems/0075-sort-colors/solution.cpp
+++ problems/0075-sort-colors/solution.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int a=0, b=0,c=0;
-        for(auto i: nums) {
-            if(i==0) a++;
-            else if (i==1) b++;
-            else c++;
-        }
-        for(int i=0;i<a;i++) {
-            nums[i] = 0;
-        }
-        for(int i=a; i<a+b; i++) {
-            nums[i] = 1;
-        }
-        for(int i=a+b; i<nums.size(); i++) {
-            nums[i] = 2;
+        int low = 0, mid = 0, high;
+        high = nums.size() - 1;
+        while(mid <= high) {
+            switch(nums[mid]) {
+                case 0:
+                    swap(nums[low], nums[mid]);
+                    low++;
+                    mid++;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid], nums[high]);
+                    high--;
+            }
         }
     }
 };
