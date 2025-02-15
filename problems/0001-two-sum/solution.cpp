4cd2c372cdaa0c0832f442c0fc21b18df53deae3
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2025-02-15 03:25:14 +0000

    Sync LeetCode submission Runtime - 15 ms (52.27%), Memory - 16.2 MB (6.87%)

---
+++ problems/0001-two-sum/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans = {0,1};
+        int temp,l=nums.size();
+        map<int,int> map;
+        for(int i=0;i<l; i++) {
+            map[nums[i]] = i;
+        }
+        for (int i=0; i<l; i++) {
+            temp = target-nums[i];
+            if(map.count(temp) && map[temp] != i) {
+                ans[0] = i;
+                ans[1] = map[temp];
+            }
+        }
+        return ans;
+    }
+};
