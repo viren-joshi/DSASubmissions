1a1aeabea9f3b8331a63191ab5e23c9cb038107f
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2025-08-24 01:30:58 +0000

    Sync LeetCode submission Runtime - 3 ms (25.68%), Memory - 60.2 MB (23.29%)

---
+++ problems/1586-longest-subarray-of-1s-after-deleting-one-element/solution.cpp
@@ -0,0 +1,41 @@
+/*
+
+As per the hint, we need to maintain a sliding window with atleast one 0 in it.
+
+We do that having two pointers (left and right). 
+right is the same variable we iterate the array with, so we increase the size of the window 
+every iteration
+
+After increasing the window, we see check for a new zero, if found,
+we remove the older one by decreasing the window size (increment left)
+
+Hence, for every iteration, 
+the ans, the maximum no. of ones after deleting one element is (size of window - no. of zeroes)
+
+There is one edge case, that if the array contains all 1s, 
+then the ans is len(arr) - 1 as one element should always be deleted.
+
+*/
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int len = nums.size();
+        int left = 0;
+        int zero = 0;
+        int ans = 0;
+        for(int right = 0; right < len; right++){
+            if(nums[right] == 0) zero++;
+            while(zero > 1) {
+                if(nums[left] == 0) zero--;
+                left++;
+            }
+            ans = max(ans, right - left + 1 - zero);
+        }
+
+        if(ans == len)
+            return ans - 1;
+            return ans;
+
+    }
+};
