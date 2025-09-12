c890d87c0669b1689e73f99658cc682821e9a1c3
Viren Joshi <73399290+viren-joshi@users.noreply.github.com>
2025-09-12 03:12:36 +0000

    Sync LeetCode submission Runtime - 1 ms (91.79%), Memory - 19.1 MB (25.75%)

--- problems/3462-vowels-game-in-a-string/solution.cpp
+++ problems/3462-vowels-game-in-a-string/solution.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
 
-    bool isVowel(char c) {
-        return c =='a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
-    }
-
     bool doesAliceWin(string s) {
         if(s.size() == 0) return false;
-        int countVowels = 0;
         for(char c: s) {
-            if(isVowel(c)) return true;
+            if(c =='a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') return true;
         }
         return false;
     }
